b30ddee705ce5a2de9530c4975355b932de3457a
mubasshir <mubasshirkamali2009@gmail.com>
2026-05-03 01:55:09 +0600

    react hook added

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,35 +1,36 @@
 "use client"
 import Link from 'next/link';
 import React from 'react';
+import { useForm } from 'react-hook-form';
 
 const LoginPage = () => {
+const {register,
+    handleSubmit , formState: {errors} } =useForm()
 
-
-const handleLoginfucn= (e) => {
-   e.preventDefault();
-   const email = e.target.email.value ;
-   const password = e.target.password.value ;
-   console.log(email,password)
+const handleLoginfucn= (data) => {
+console.log(data,'data');
 }
 
 
-
+console.log(errors,"errors")
 
 
     return (
         <div className='container mx-auto min-h[80vh] flex justify-center items-center'>
             <div className='py-20 px-10 rounded-xl bg-green-800 my-50'>
  <h2 className='text-3xl font-bold mb-6 text-white'>Login your account</h2>
- <form action="" onSubmit={handleLoginfucn} className='space-y-5'>
+ <form action="" onSubmit={handleSubmit(handleLoginfucn)} className='space-y-5'>
     <fieldset className="fieldset">
   <legend className="fieldset-legend text-white">Your Email</legend>
-  <input name='email' type="email" className="input" placeholder="Your Email" />
-  
+  <input {...register("email" ,  {required: "email field requard"})} type="email" className="input" placeholder="Your Email" />
+  {errors.email && <p className='text-red-500 
+   text-lg'>{errors.email.message}</p> }
 </fieldset>
     <fieldset className="fieldset">
   <legend className="fieldset-legend text-white">Your Password</legend>
-  <input name='password' type="Password" className="input" placeholder="Your Password" />
-  
+  <input {...register("password" , {required: "Password field requard"} )} type="Password" className="input" placeholder="Your Password"  />
+  {errors.password && <p className='text-red-500 
+   text-lg'> {errors.password.message} </p>}
 </fieldset>
 <button className="btn btn-success mx-auto  w-full">Login</button>
  </form>
